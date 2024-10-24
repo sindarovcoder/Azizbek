4280cb08d3ae73f9557d8f0e094dcc1737f9cc87
safed <sindarovsardor9@gmail.com>
2024-10-25 00:33:12 +0500

    init change

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "10/12/",
+    dayMonth = "12/12/",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
